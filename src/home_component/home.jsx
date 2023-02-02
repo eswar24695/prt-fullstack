3be4f753d1527cdf0f08f68883795ecf7b5ff82a
eswar24695 <eswar24695@gmail.com>
2023-02-02 23:19:00 +0530

    timer

--- src/home_component/home.jsx
+++ src/home_component/home.jsx
@@ -7,85 +7,128 @@ import "./home.css"
 
 const Home = () => {
     const navigate = useNavigate()
-    const [getalltasks,setgetalltask]=useState([])
-    const [inputtext,setInputtext]=useState({Activity:""})
-    const user=JSON.parse(window.localStorage.getItem("userData"));
-    
+    const [getalltasks, setgetalltask] = useState([])
+    const [inputtext, setInputtext] = useState({ Activity: "" })
+    const user = JSON.parse(window.localStorage.getItem("userData"));
+    const [start, setStart] = useState(false);
+    const [time, setTime] = useState(0);
+    const [buttonText, setButtonText] = useState("Start");
+
     const token = localStorage.getItem("token");
-    const config={
-        headers:{
-            token:token
+    const config = {
+        headers: {
+            token: token
         }
     }
-        useEffect(()=>{
-            axios.get("https://todobackend-8h8i.onrender.com/tasks/all",config)
-            .then((res)=>{
+
+
+    useEffect(() => {
+        let interval = null;
+        if (start) {
+            interval = setInterval(() => {
+                setTime(time => time + 1);
+            }, 1000);
+        } else if (!start && time !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [start, time]);
+
+    const handleStart = () => {
+        setStart(true);
+        setButtonText("End");
+    };
+
+    const handleStop = () => {
+        setStart(false);
+        setButtonText("Start");
+    };
+
+    const handlePause = () => {
+        setStart(false);
+        setButtonText("Resume");
+    };
+
+    const formatTime = time => {
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const seconds = time % 60;
+        return `${hours}:${minutes}:${seconds}`;
+    };
+
+    useEffect(() => {
+        axios.get("https://todobackend-8h8i.onrender.com/tasks/all", config)
+            .then((res) => {
                 console.log(res.data.data)
                 setgetalltask(res.data.data);
             })
-            .catch((e)=>{
+            .catch((e) => {
                 console.log(e.message);
             })
-        },[])
-        const sendtask=async(inputtext)=>{
-            await axios.post("https://todobackend-8h8i.onrender.com/tasks/addtask",inputtext,config)
-            .then((res)=>{
-                setgetalltask([...getalltasks,res.data.data]);
+    }, [])
+    const sendtask = async (inputtext) => {
+        await axios.post("https://todobackend-8h8i.onrender.com/tasks/addtask", inputtext, config)
+            .then((res) => {
+                setgetalltask([...getalltasks, res.data.data]);
                 console.log(res.data.data);
                 document.location.reload();
 
             })
-            .catch((e)=>{
+            .catch((e) => {
                 console.log(e);
             })
-        } 
-        
-return(
-    <>
-        <div className="home">
-            <div className="header">
-                <div className="username">{user.name.split("@")[0]}</div>
-            </div>
-            <div className="home-body">
-                <div className="sidebar">
-                    <h1 style={{"marginLeft":"5rem"}}>History</h1>
-                    <button className="deletebutton" onClick={() => { localStorage.clear(); navigate("/") }}>Logout</button>
-                </div>
-                <div className="activity-main">
-                    <div className="input">
-                        <input className="searchbar" type="text" name="Activity" onChange={(e)=>{setInputtext({inputtext, Activity:e.target.value})}}/>
-                        <button onClick={()=>{sendtask(inputtext)}}>Add new activity</button>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Activity</th>
-                                <th>Status</th>
-                                <th>Time taken</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {getalltasks?.map((item)=>{
-                                return(
-                                    <tr key={item._id}>
-                                        <td>{item.Activity}</td>
-                                        <td>{item.status}</td>
-                                        <td>time</td>
-                                        <td>
-                                            <button>Start</button>
-                                            <button>End</button>
-                                            <button>Pause</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
+    }
 
-                        </tbody>
-                    </table>
+    return (
+        <>
+            <div className="home">
+                <div className="header">
+                    <div className="username">{user.name.split("@")[0]}</div>
+                </div>
+                <div className="home-body">
+                    <div className="sidebar">
+                        <h1 style={{ "marginLeft": "5rem" }}>History</h1>
+                        <button className="deletebutton" onClick={() => { localStorage.clear(); navigate("/") }}>Logout</button>
+                    </div>
+                    <div className="activity-main">
+                        <div className="input">
+                            <input className="searchbar" type="text" name="Activity" onChange={(e) => { setInputtext({ inputtext, Activity: e.target.value }) }} />
+                            <button onClick={() => { sendtask(inputtext) }}>Add new activity</button>
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Activity</th>
+                                    <th>Status</th>
+                                    <th>Time taken</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {getalltasks?.map((item) => {
+                                    return (
+                                        <tr key={item._id}>
+                                            <td>{item.Activity}</td>
+                                            <td>{item.status}</td>
+                                            <td>{formatTime(time)}</td>
+                                            <td>
+                                                {start ? (
+                                                    <button onClick={handlePause}>Pause</button>
+                                                ) : (
+                                                    <button onClick={handleStart}>{buttonText}</button>
+                                                )}
+                                                {start && <button onClick={handleStop}>End</button>}
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-)}
+        </>
+    )
+}
 export default Home;
